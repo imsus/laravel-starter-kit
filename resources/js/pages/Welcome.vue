22879e510ff5a01fe9804716a7e51d221ec19d73
Imam Susanto <abc.imams@gmail.com>
2026-01-18 23:19:43 +0700

    refactor: use canonical Tailwind classes in Welcome page

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -18,10 +18,10 @@ import DarkModeToggle from "@/components/DarkModeToggle.vue";
       class="flex w-full items-center justify-center opacity-100 transition-opacity duration-750 lg:grow starting:opacity-0"
     >
       <main
-        class="flex w-full max-w-[335px] flex-col-reverse overflow-hidden rounded-lg lg:max-w-4xl lg:flex-row"
+        class="flex w-full max-w-[21rem] flex-col-reverse overflow-hidden rounded-lg lg:max-w-4xl lg:flex-row"
       >
         <div
-          class="flex-1 rounded-br-lg rounded-bl-lg bg-white p-6 pb-12 text-[13px] leading-[20px] shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] lg:rounded-tl-lg lg:rounded-br-none lg:p-20 dark:bg-[#161615] dark:text-[#EDEDEC] dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d]"
+          class="flex-1 rounded-br-lg rounded-bl-lg bg-white p-6 pb-12 text-sm leading-5 shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] lg:rounded-tl-lg lg:rounded-br-none lg:p-20 dark:bg-[#161615] dark:text-[#EDEDEC] dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d]"
         >
           <h1 class="mb-1 font-medium">Let's get started</h1>
           <p class="mb-2 text-[#706f6c] dark:text-[#A1A09A]">
@@ -117,7 +117,7 @@ import DarkModeToggle from "@/components/DarkModeToggle.vue";
           </ul>
         </div>
         <div
-          class="relative -mb-px aspect-335/376 w-full shrink-0 overflow-hidden rounded-t-lg bg-[#fff2f2] lg:mb-0 lg:-ml-px lg:aspect-auto lg:w-[438px] lg:rounded-t-none lg:rounded-r-lg dark:bg-[#1D0002]"
+          class="relative -mb-px aspect-335/376 w-full shrink-0 overflow-hidden rounded-t-lg bg-[#fff2f2] lg:mb-0 lg:-ml-px lg:aspect-auto lg:w-[27.5rem] lg:rounded-t-none lg:rounded-r-lg dark:bg-[#1D0002]"
         >
           <svg
             class="w-full max-w-none translate-y-0 text-[#F53003] opacity-100 transition-all duration-750 dark:text-[#F61500] starting:translate-y-6 starting:opacity-0"
@@ -152,7 +152,7 @@ import DarkModeToggle from "@/components/DarkModeToggle.vue";
             />
           </svg>
           <svg
-            class="relative -mt-[4.9rem] -ml-8 w-[448px] max-w-none lg:-mt-[6.6rem] lg:ml-0 dark:hidden"
+            class="relative -mt-[4.9rem] -ml-8 w-[28rem] max-w-none lg:-mt-[6.6rem] lg:ml-0 dark:hidden"
             viewBox="0 0 440 376"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
@@ -476,7 +476,7 @@ import DarkModeToggle from "@/components/DarkModeToggle.vue";
             </g>
           </svg>
           <svg
-            class="relative -mt-[4.9rem] -ml-8 hidden w-[448px] max-w-none lg:-mt-[6.6rem] lg:ml-0 dark:block"
+            class="relative -mt-[4.9rem] -ml-8 hidden w-[28rem] max-w-none lg:-mt-[6.6rem] lg:ml-0 dark:block"
             viewBox="0 0 440 376"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
